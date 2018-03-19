ea2e6f34231d584c560ced08d49eb133d7fd5627
krasimirpetrov <krasi.p@24cfd.com>
2018-03-19 20:26:44 +0200

    Initial commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/3d/20a54fc99d2b001812bba90c13fcef10
@@ -0,0 +1,7 @@
+
+public class MainServletextends HttpServlet {
+	   @Override
+	   public void doGet(HttpServletRequest request, HttpServletResponse response)
+	               throws IOException, ServletException {
+	      }
+	   }
---
+++ hackatonkm/src/main/java/hackatonkm/MainServlet.java
@@ -0,0 +1,22 @@
+package hackatonkm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String res = request.getParameter("krasi");
+		res = "3";
+		res.concat("3");
+	}
+}
