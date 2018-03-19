e1fa506f79659aace7ce41deda71b755f3bac848
Mihail Tsonev <mihail.tsonev@sap.com>
2018-03-19 20:54:31 +0200

    Fix imports

--- src/main/java/hackatonkm/MainServlet.java
+++ src/main/java/hackatonkm/MainServlet.java
@@ -1,6 +1,7 @@
 package hackatonkm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,19 +9,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
 
-	@Override
-		public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		String res = request.getParameter("krasi");
-		response.setContentType("text/html");
-		// Get the printwriter object from response to write the required json object to the output stream      
-		PrintWriter out = response.getWriter();
-		// Assuming your json object is *jsonObject*, perform the following, it will return your json object  
-		out.print(res + "A");
-		out.flush();
-	}
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String res = request.getParameter("krasi");
+        response.setContentType("text/html");
+        // Get the printwriter object from response to write the required json object to the output stream
+        PrintWriter out = response.getWriter();
+        // Assuming your json object is *jsonObject*, perform the following, it will return your json object
+        out.print(res + "A");
+        out.flush();
+    }
 }
