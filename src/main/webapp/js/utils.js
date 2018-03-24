53e89b667e5070a2bff6f22ba4eb34a77286c82c
krasimirpetrov <krasi.p@24cfd.com>
2018-03-24 19:46:32 +0200

    idiot

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -30,3 +30,17 @@
 	$("#login").click(function(){
 		register();
 	})
+
+	function addVideoInDialog(id, stream) {
+		var newDiv = $(document.createElement('div')); 
+		newDiv.html('<video id="' + id + '" autoplay style="width:100%; height:100%;  overflow:hidden;"></video>');
+		newDiv.dialog({width: 440,height:320, open: function(){
+			$(this).css('overflow', 'hidden')
+		}});
+		
+		var myVideoTag = $('#' + id)[0];
+		myVideoTag.srcObject = stream;		
+
+	}
+	
+	
