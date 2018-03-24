a7a0ace33c59532a6a46ba6d4668ac31922df2ca
krasimirpetrov <krasi.p@24cfd.com>
2018-03-24 19:44:07 +0200

    login

--- src/main/webapp/js/utils.js
+++ src/main/webapp/js/utils.js
@@ -10,17 +10,23 @@
 			  dataType: "json"
 			});
 	}
-		
-	function addVideoInDialog(id, stream) {
-		var newDiv = $(document.createElement('div')); 
-		newDiv.html('<video id="' + id + '" autoplay style="width:100%; height:100%;  overflow:hidden;"></video>');
-		newDiv.dialog({width: 440,height:320, open: function(){
-			$(this).css('overflow', 'hidden')
-		}});
-		
-		var myVideoTag = $('#' + id)[0];
-		myVideoTag.srcObject = stream;		
 
+	
+	// Registration
+	function register() {
+		var login = {};
+		login.cmd = "register";
+		login.userName = $("#userName").val();
+		login.password = $("#password").val();
+		login.role = $("#role").val();
+		
+		sendMessage(login,function(data){
+			alert("Successfully registered")
+		}, function(er){
+			alert("Error is: " + er)
+		});
 	}
 	
-
+	$("#login").click(function(){
+		register();
+	})
